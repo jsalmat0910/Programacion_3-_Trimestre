c5762c19801a35277e5847c407e1f51c5a7175a7
jsalmat0910 <jsalmat0910@g.educaand.es>
2022-04-28 10:31:08 +0200

    JRSM-Programacion-Tema_6-Parte-b.

--- Parte b/VehiculosNuevos-CSV/src/main/java/es/juanramonsalgueromateos/vehiculosnuevos/csv/App.java
+++ Parte b/VehiculosNuevos-CSV/src/main/java/es/juanramonsalgueromateos/vehiculosnuevos/csv/App.java
@@ -11,6 +11,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -24,7 +26,9 @@ public class App extends Application {
     Coche coche;
     Datos datos = new Datos();
     ArrayList listaPaises = new ArrayList();
-    static int textoActual = 0;
+    ArrayList listaTodosPaises = new ArrayList();
+    static int totalPaguinas = 0;
+    String[] valores;
     
     @Override
     public void start(Stage stage) {
@@ -59,7 +63,7 @@ public class App extends Application {
             
          // Repetir mientras no se llegue al final del fichero
             while(texto != null) {
-                String[] valores = texto.split(",");
+                valores = texto.split(",");
                 
              // Introduccion de los valores dependiendo de las columnas.
                 coche = new Coche(valores[0], valores[1], Integer.parseInt(valores[2]), Float.parseFloat(valores[3]), Float.parseFloat(valores[4]), Float.parseFloat(valores[5]), Float.parseFloat(valores[6]), Float.parseFloat(valores[7]));
@@ -76,10 +80,9 @@ public class App extends Application {
                 }else{
                     //System.out.println("No se introduce Pais en el Array");
                 }
-                
+                //listaTodosPaises.add(coche.getPais());
                 datos.getDatos().add(coche);
                 
-                System.out.println();
                 
                 
              // Actualizar variable del pais anterior.
@@ -87,6 +90,7 @@ public class App extends Application {
                 
              // Salto de línea.
                 texto = br.readLine();
+                totalPaguinas += 1;
                 
             }
         }
@@ -118,23 +122,38 @@ public class App extends Application {
      // Crea pantalla horizontal en la que se muestra el indice de los datos que se van a mostrar.
         HBox hbox1 = new HBox();
         hbox1.setAlignment(Pos.CENTER);
-        hbox1.setSpacing(10);
         
-        Label indicePais = new Label("País");
+        Label indicePais = new Label(" País ");
+        indicePais.setAlignment(Pos.TOP_CENTER);
+        indicePais.setStyle("-fx-border-color:GRAY;");
         
-        Label indiceCodigo = new Label("Código");
+        Label indiceCodigo = new Label(" Código ");
+        indiceCodigo.setAlignment(Pos.TOP_CENTER);
+        indiceCodigo.setStyle("-fx-border-color:GRAY;");
         
-        Label indiceAño = new Label("Año");        
+        Label indiceAño = new Label(" Año ");        
+        indiceAño.setAlignment(Pos.TOP_CENTER);
+        indiceAño.setStyle("-fx-border-color:GRAY;");
         
-        Label indiceTipo1 = new Label("Híbrido Enchufable");        
+        Label indiceTipo1 = new Label(" Híbrido Enchufable ");        
+        indiceTipo1.setAlignment(Pos.TOP_CENTER);
+        indiceTipo1.setStyle("-fx-border-color:GRAY;");
         
-        Label indiceTipo2 = new Label("Híbrido Completo");        
+        Label indiceTipo2 = new Label(" Híbrido Completo ");        
+        indiceTipo2.setAlignment(Pos.TOP_CENTER);
+        indiceTipo2.setStyle("-fx-border-color:GRAY;");
         
-        Label indiceTipo3 = new Label("Batería Eléctrica");        
+        Label indiceTipo3 = new Label(" Batería Eléctrica ");        
+        indiceTipo3.setAlignment(Pos.TOP_CENTER);
+        indiceTipo3.setStyle("-fx-border-color:GRAY;");
         
-        Label indiceTipo4 = new Label("Gasolina");        
+        Label indiceTipo4 = new Label(" Gasolina ");        
+        indiceTipo4.setAlignment(Pos.TOP_CENTER);
+        indiceTipo4.setStyle("-fx-border-color:GRAY;");
         
-        Label indiceTipo5 = new Label("Diesel");
+        Label indiceTipo5 = new Label(" Diesel ");
+        indiceTipo5.setAlignment(Pos.TOP_CENTER);
+        indiceTipo5.setStyle("-fx-border-color:GRAY;");
         
         hbox1.getChildren().add(indicePais);
         hbox1.getChildren().add(indiceCodigo);
@@ -148,41 +167,58 @@ public class App extends Application {
      // Crear segunda pantalla horizontal en la que se muestran los datos que hemos seleccionado.
         HBox hbox2 = new HBox();
         hbox2.setAlignment(Pos.CENTER);
-        hbox2.setSpacing(10);
 
      // Creacion del label donde se muestra la informacion seleccionada.
         Label textoPais = new Label();
-        
+        textoPais.setAlignment(Pos.TOP_CENTER);
+        textoPais.setStyle("-fx-border-color:GRAY;");
         
         Label textoCodigo = new Label();
-        
+        textoCodigo.setAlignment(Pos.TOP_CENTER);
+        textoCodigo.setStyle("-fx-border-color:GRAY;");
         
         Label textoAño = new Label();
-        
+        textoAño.setAlignment(Pos.TOP_CENTER);
+        textoAño.setStyle("-fx-border-color:GRAY;");
         
         Label textoTipo1 = new Label( );
-        
+        textoTipo1.setAlignment(Pos.TOP_CENTER);
+        textoTipo1.setStyle("-fx-border-color:GRAY;");
         
         Label textoTipo2 = new Label();
-        
+        textoTipo2.setAlignment(Pos.TOP_CENTER);
+        textoTipo2.setStyle("-fx-border-color:GRAY;");
         
         Label textoTipo3 = new Label();
-        
+        textoTipo3.setAlignment(Pos.TOP_CENTER);
+        textoTipo3.setStyle("-fx-border-color:GRAY;");
         
         Label textoTipo4 = new Label();
-        
+        textoTipo4.setAlignment(Pos.TOP_CENTER);
+        textoTipo4.setStyle("-fx-border-color:GRAY;");
         
         Label textoTipo5 = new Label();
-
+        textoTipo5.setAlignment(Pos.TOP_CENTER);
+        textoTipo5.setStyle("-fx-border-color:GRAY;");
+        
      // Creacion de la caja donde se selecciona lo deseado de la lista.
         ComboBox<String> cajaPais = new ComboBox(FXCollections.observableList(listaPaises));
         cajaPais.setOnAction((ActionEvent t) -> {
          // Valor seleccionado del combo box.
             String valorCaja = cajaPais.getValue();
-            if (valorCaja.equals(datos.getDatos())){
-                textoPais.setText(datos.getDatos().get(textoActual).getPais());
-                textoCodigo.setText(datos.getDatos().get(textoActual).getCodigo());
-                
+            
+
+            for(int nDato = 0; nDato < totalPaguinas; nDato++)  {
+                if(valorCaja.equals(datos.getDatos().get(nDato).getPais())){
+                    textoPais.setText(datos.getDatos().get(nDato).getPais());
+                    textoCodigo.setText(datos.getDatos().get(nDato).getCodigo());
+                    textoAño.setText(String.valueOf(datos.getDatos().get(nDato).getAño()));
+                    textoTipo1.setText(String.valueOf(datos.getDatos().get(nDato).getHibridoEnchufable()));
+                    textoTipo2.setText(String.valueOf(datos.getDatos().get(nDato).getHibridoCompleto()));
+                    textoTipo3.setText(String.valueOf(datos.getDatos().get(nDato).getBateriaElectrica()));
+                    textoTipo4.setText(String.valueOf(datos.getDatos().get(nDato).getGasolina()));
+                    textoTipo5.setText(String.valueOf(datos.getDatos().get(nDato).getDiesel()));
+                }            
             }
         });
         
@@ -195,6 +231,8 @@ public class App extends Application {
         hbox2.getChildren().add(textoTipo4);
         hbox2.getChildren().add(textoTipo5);
         
+//        TableView<Datos> tablaDatos = new TableView();
+//        TableColumn<Datos, String> colPais = new TableColumn("Pais");
 
      // Mostrar en pantalla.
         root.getChildren().add(cajaPais);
---
+++ Parte b/VehiculosNuevos-CSV/src/main/java/es/juanramonsalgueromateos/vehiculosnuevos/csv/UtilCSV.java
@@ -0,0 +1,14 @@
+package es.juanramonsalgueromateos.vehiculosnuevos.csv;
+
+
+public class UtilCSV {
+    
+    public static void LeerDatosCsv(){
+        
+    }
+    
+    public static void GuardarDatosCsv(){
+        
+    }
+    
+}
